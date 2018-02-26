e850226f65cd5ede6be1bc770f34863d2644d05e
SerpRifa <serprifa@gmail.com>
2018-02-26 19:34:25 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
 import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
+import {Redirect, Route, Switch, Link, NavLink} from 'react-router-dom';
+import Home from './Home';
+import Auth from './Auth';
+import Public from './Public';
+import Private from './Private';
+
 
 class App extends Component {
   state = {
     isAuthorized
-  };
+  }
 
   componentDidMount() {
     addListener(this.handleAuthorize);
@@ -17,10 +23,31 @@ class App extends Component {
 
   handleAuthorize = isAuthorized => {
     this.setState({isAuthorized});
-  };
+  }
 
   render() {
-    return null;
+    const {isAuthorized} = this.state;
+    //const {location:{pathname}} = this.props;
+    return <div>        
+        <nav>
+          <ul>
+            <li> <NavLink to={'/private'}>Секретная страница</NavLink>  </li>
+            <li> <NavLink to={'/auth'}>Войти</NavLink></li>
+            <li><NavLink to={'/public'}>Публичная страница</NavLink></li>
+            <li><NavLink to={'/'}>Главная</NavLink></li>
+          </ul>
+        </nav>
+        <hr/>
+        {/* {!isAuthorized && } */}
+        <Switch>
+          {isAuthorized === false && <Redirect from="/private" to="/auth" />}
+          {isAuthorized && <Route path="/private" exact component={Private} />}
+          <Route path="/public" exact component={Public} />
+          <Route path="/auth" exact component={Auth} />
+          <Route path="/" exact component={Home} />  
+          {isAuthorized === false && <Redirect from="/*" to="/" />      }  
+        </Switch>
+      </div>;
   }
 }
 
--- src/Auth.js
+++ src/Auth.js
@@ -1,8 +1,41 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+import {authorizeUser, isAuthorized} from './AuthorizeApi';
 
 class Auth extends Component {
+  state={
+    email: '',
+    password: '',
+    isAuthorized: false,
+    clicked: false
+  }
+  handleEmail = events => {
+    const {value} = events.target;
+    this.setState({email: value});
+  }
+  handlerPassword = events => {
+    const {value} = events.target;
+    this.setState({password: value});
+  }
+  handleSubmit = () =>{
+    const {email, password} = this.state;
+    const isAuthorized = authorizeUser(email, password);
+    this.setState({isAuthorized: isAuthorized, clicked: true});
+  }
+
   render() {
-    return null;
+    const {email, password, isAuthorized, clicked} = this.state;
+    const {onClick} = this.props;
+
+    return <div>
+        <div>
+          <input name="email" onChange={this.handleEmail} placeholder="email" />
+          <input name="password" onChange={this.handlerPassword} placeholder="password" />
+        </div>
+        {clicked && <p className="error">Неверный пароль и/или почта.</p>}        
+        <button name="Submit" onClick={this.handleSubmit} >Submit</button>
+        {isAuthorized && <Redirect to="/" />}
+      </div>;
   }
 }
 
